bcfd1c47c9d32d93d46763d1a0a1fe2e7120498c
KennethAC4 <147218634+KennethAC4@users.noreply.github.com>
2023-11-25 11:49:23 -0400

    Delete ejemplowf directory

--- ejemplowf/anuncio.vista.inc.php
+++
@@ -1,5 +0,0 @@
-<!-- El estudiante debe presentar los siguientes documentos:<br>
--fotocopia de CI<br>
--pago de matricula<br>
--titulo de bachiller<br> -->
-Check
--- ejemplowf/listado.vista.inc.php
+++
@@ -1,9 +0,0 @@
-<?php  
-    $resultadof=mysqli_query($link, "select * from academico.alumnos where ci='23'");
-    $filaf=mysqli_fetch_array($resultadof);
-
-    $ci=$filaf["ci"];
-    $nombre=$filaf["nombre"];
-?>
-<input type="text" name = "ci" value = "<?php  echo $ci;?>"/>
-<input type="text" name = "nombre" value = "<?php  echo $nombre;?>"/>
